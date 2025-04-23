6e99f1c888dbaf8c9b45e33c2fdbc426e6d1f832
dCurre <davidecurreli@gmail.com>
2025-04-23 12:32:28 +0200

    java into python

--- README.md
+++
@@ -1,1 +0,0 @@
-# spese-ms
--- src/main/java/com/dcapps/spese/repository/ExpensesListsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dcapps.spese.repository;
-
-import com.dcapps.spese.repository.entity.ExpensesLists;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpensesListsRepository extends JpaRepository<ExpensesLists, Long> {
-}
