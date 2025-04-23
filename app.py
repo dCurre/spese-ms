c02d4861b81498cf1874d7e3b254c525097e586b
dCurre <davidecurreli@gmail.com>
2025-04-23 18:44:07 +0200

    logger

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
+import logging
 from flask import Flask
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
     return "Hello from Flask on Render!"
+
+print(f"__name__ is: {__name__}")
+logger.info(f"__name__ is: {__name__}")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local testing
