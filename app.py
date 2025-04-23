ce1c39fc12cbc9f734173cfd5d79c4c47a657412
dCurre <davidecurreli@gmail.com>
2025-04-23 19:06:27 +0200

    refactor

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-print("DAVIDE Loading app.py file")
-
-import os
-import logging
 from flask import Flask
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
     return "Hello from Flask on Render!"
-
-print(f"__name__ is: {__name__}")
-logger.info(f"__name__ is: {__name__}")
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local testing
-    app.run(host='0.0.0.0', port=port)
--- wsgi.py
+++
@@ -1,6 +0,0 @@
-# wsgi.py
-from app import create_app
-
-print("Creating Flask app...")  # DEBUG line
-app = create_app()
-print("App created.")           # DEBUG line
