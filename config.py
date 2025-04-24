b3b4079d21adee87cd483ea58d186aaef19f6858
dCurre <davidecurreli@gmail.com>
2025-04-24 15:19:28 +0200

    env variables

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.zclxrrjkcteilzlzlqhq:c1rmee94%21SPESE@aws-0-eu-central-1.pooler.supabase.com:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TRY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
