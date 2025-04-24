e16961da6ba707cbd9da2b8be2eafe1d2cd53c72
dCurre <davidecurreli@gmail.com>
2025-04-24 11:48:04 +0200

    test

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:c1rmee94!SPESE@db.zclxrrjkcteilzlzlqhq.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:c1rmee94%21SPESE@db.zclxrrjkcteilzlzlqhq.supabase.co:5432/postgres?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
