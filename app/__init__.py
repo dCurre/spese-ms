f995e0fd7fa0097657cefd0d161da017d472f7a5
dCurre <davidecurreli@gmail.com>
2025-05-03 13:09:54 +0200

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from config import Config
 def create_app():
     app = Flask(__name__)  # Move this inside the function
 
-    CORS(app)  # Enables CORS for all origins
+    CORS(app, origins=["https://spesedc.web.app", "http://localhost:4200"])
 
     app.config.from_object(Config)
 
